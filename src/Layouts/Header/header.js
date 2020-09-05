0ad3d068e0bc187b510a10fbf93fe280310111a9
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2020-09-05 15:58:56 +0700

    First setupt layout home component

---
+++ src/Layouts/Header/header.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <div className="row">
+        <div className="card hovercard">
+          <div className="card-background"> </div>
+          <div className="useravatar">
+            <img alt="cybersoft.edu.vn" src="images/cybersoft.png" />
+          </div>
+          <div className="card-info">
+            <span className="card-title">
+              CyberSoft.edu.vn - Đào tạo chuyên gia lập trình - Dự án thử đồ
+              trực tuyến - Virtual Dressing Room
+            </span>
+          </div>
+        </div>
+      </div>
+      <hr className="style13" />
+    </div>
+  );
+};
+
+export default HeaderComponent;
---
+++ src/Screens/Home/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HeaderComponent from "../../Layouts/Header/header";
+import FooterComponent from "../../Layouts/Footer/footer";
+
+const Home = () => {
+  return (
+    <div>
+      <HeaderComponent />
+      <FooterComponent />
+    </div>
+  );
+};
+
+export default Home;
